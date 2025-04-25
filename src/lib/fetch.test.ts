95472aa88b492e81884a4d12f7e9c01d593d0ebd
Nicolas Vitaterna <nvitaterna@users.noreply.github.com>
2025-04-25 09:23:20 -0400

    chore: update tag paths for pr check
    * chore(build): fix tag release paths, closes #16
    
    * chore(build): update paths for pr check

---
+++ src/lib/fetch.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest';
+import { fetchSchedule, fetchVersions } from './fetch.js';
+
+describe('fetch', () => {
+  describe('fetchSchedule', () => {
+    it('should fetch from the nodejs schedule', async () => {});
+  });
+});
